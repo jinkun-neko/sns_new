034e8c58daaa874fdf28a432d89059faf290df9e
jinkun <jin2jin700@gmail.com>
2022-10-10 16:21:52 +0900

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
-  def new
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new  #新しいユーザーを作るフォームのため。
+    @user = User.new
+  end
+  
+  def create  #newで送られたデータがcreateに来る。
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the SNS APP!"
+      redirect_to @user
+    else
+      flash[:danger] = "Why can't I login? ?"
+      render 'new'
+    end
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                :password_confirmation)
   end
 end
